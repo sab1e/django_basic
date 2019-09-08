120a31f991b47b90631395b080c1a77d4418cd67
sab1e <antony.sobolev@gmail.com>
2019-09-09 00:26:56 +0500

    1. Добавить к модели корзины методы и вывести в меню количество товаров и их полную стоимость. 2. Реализовать механизм просмотра содержимого корзины и удаления товаров из нее. 3. Реализовать просмотр товара, скорректировать адреса в каталоге и на главной странице так, чтобы при нажатии на товар появлялась страница просмотра. Добавление товара в корзину теперь должно быть только с этой страницы. 4. Защитить доступ к корзине декоратором @login_required. 5*. Реализовать асинхронное редактирование количества товаров в корзине при помощи AJAX. 6*. Реализовать механизм вывода случайного товара на странице «Горячее предложение», которая появляется при входе в каталог.

--- interiorshop/mainapp/views.py
+++ interiorshop/mainapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from django.db.models import Sum
-import datetime
 from .models import ProductCategory, Product
 from basketapp.models import Basket
+import datetime
+import random
 
 
 def main(request):
@@ -18,11 +18,10 @@ def main(request):
 def products(request, pk=None):
     title = 'Products'
     products_category = ProductCategory.objects.all()
+    basket = get_basket(request.user)
 
-    basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
-        basket_count = basket.aggregate(Sum('quantity'))
 
     if pk is not None:
         if pk == 0:
@@ -37,21 +36,35 @@ def products(request, pk=None):
             'products_category': products_category,
             'category': category,
             'products': products,
-            'basket_count': basket_count
+            'basket': basket
         }
         return render(request, 'products_list.html', content)
 
-    products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     content = {
         'title': title,
         'products_category': products_category,
-        'products': products,
-        'basket_count': basket_count
+        'hot_product': hot_product,
+        'same_products': same_products,
+        'basket': basket
     }
 
     return render(request, 'products.html', content)
 
+
+def product(request, pk):
+    title = 'Товар'
+
+    content = {
+        'title': title,
+        'products_category': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'product.html', content)
 
 
 def contact(request):
@@ -81,4 +94,23 @@ def contact(request):
 
     content = {'title': title, 'visit_date': visit_date, 'locations': locations}
     return render(request, 'contact.html',  content)
-# Create your views here.
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).\
+                                            exclude(pk=hot_product.pk)[:3]
+
+    return same_products
