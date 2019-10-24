120610100401c14b3f13e4f5ca38395c48777de5
sab1e <antony.sobolev@gmail.com>
2019-10-24 21:33:14 +0500

    cached models Basket

--- interiorshop/basketapp/models.py
+++ interiorshop/basketapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 class Basket(models.Model):
@@ -18,19 +19,19 @@ class Basket(models.Model):
         "return cost of all products this type"
         return self.product.price * self.quantity
 
-    @property
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     def total_quantity(self):
         "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
 
-    @property
     def total_cost(self):
         "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
 
     @staticmethod
     def get_items(user):
