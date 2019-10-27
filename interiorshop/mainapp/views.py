e36e84ca52e2fc3d74528b9a24da0f21d3e7ae04
sab1e <antony.sobolev@gmail.com>
2019-10-27 21:47:38 +0500

    fix mainapp.view

--- interiorshop/mainapp/views.py
+++ interiorshop/mainapp/views.py
@@ -17,8 +17,8 @@ import random
 
 def db_profile_by_type(prefix, type, queries):
     update_queries = list(filter(lambda x: type in x['sql'], queries))
-    print(f'db_profile{type} for {prefix}:')
-    [print(query['sql']) for query in update_queries]
+    # print(f'db_profile{type} for {prefix}:')
+    # [print(query['sql']) for query in update_queries]
 
 
 @receiver(pre_save, sender=ProductCategory)
