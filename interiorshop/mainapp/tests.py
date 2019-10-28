33b03255210c5a5106bcf27a7bd162fe58346bd2
sab1e <antony.sobolev@gmail.com>
2019-10-28 21:27:05 +0500

    change mainapp.test

--- interiorshop/mainapp/tests.py
+++ interiorshop/mainapp/tests.py
@@ -5,10 +5,10 @@ from django.core.management import call_command
 
 
 class TestMainappSmoke(TestCase):
-    # def setUp(self):
-    #     call_command('flush', '--noinput')
-    #     call_command('loaddata', 'test_db.json')
-    #     self.client = Client()
+    def setUp(self):
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
 
     def test_mainapp_urls(self):
         response = self.client.get('/')
@@ -17,15 +17,15 @@ class TestMainappSmoke(TestCase):
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
 
-        # response = self.client.get('/products/')
-        # self.assertEqual(response.status_code, 200)
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
 
-        # response = self.client.get('/products/category/0/')
-        # self.assertEqual(response.status_code, 200)
-        #
-        # for category in ProductCategory.objects.all():
-        #     response = self.client.get(f'/products/category/{category.pk}/')
-        #     self.assertEqual(response.status_code, 200)
+        response = self.client.get('/products/category/0/')
+        self.assertEqual(response.status_code, 200)
+
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/category/{category.pk}/')
+            self.assertEqual(response.status_code, 200)
 
         for product in Product.objects.all():
             response = self.client.get(f'/products/product/{product.pk}/')
