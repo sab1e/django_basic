b75f4c216e111425fa8838d59ce23bca522f1672
sab1e <antony.sobolev@gmail.com>
2019-10-28 21:22:50 +0500

    change mainapp.test

--- interiorshop/mainapp/tests.py
+++ interiorshop/mainapp/tests.py
@@ -13,13 +13,12 @@ class TestMainappSmoke(TestCase):
     def test_mainapp_urls(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        print('OK')
 
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get('/products/')
-        self.assertEqual(response.status_code, 200)
+        # response = self.client.get('/products/')
+        # self.assertEqual(response.status_code, 200)
 
         response = self.client.get('/products/category/0/')
         self.assertEqual(response.status_code, 200)
