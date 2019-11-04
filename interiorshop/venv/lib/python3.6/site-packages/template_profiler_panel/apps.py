df830281000723c632151786255b87c9196db779
sab1e <antony.sobolev@gmail.com>
2019-11-04 17:28:03 +0500

    delete trash

--- interiorshop/venv/lib/python3.6/site-packages/template_profiler_panel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TemplateProfilerPanelAppConfig(AppConfig):
-    name = "template_profiler_panel"
-    verbose_name = "Debug Toolbar Template Profiler Panel"
